dbafe13000e2aa05098dd90dde987fe0b0deed70
jampukka <janne.heikkila@nls.fi>
2018-01-12 15:17:30 +0200

    Add getJSON back to LayerFormatterWMTS with some changes

--- service-map/src/main/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterWMTS.java
+++ service-map/src/main/java/fi/nls/oskari/map/layer/formatters/LayerJSONFormatterWMTS.java
@@ -1,11 +1,14 @@
 package fi.nls.oskari.map.layer.formatters;
 
+import fi.nls.oskari.domain.map.OskariLayer;
 import fi.nls.oskari.map.geometry.ProjectionHelper;
 import fi.nls.oskari.util.JSONHelper;
 import fi.nls.oskari.wmts.domain.TileMatrixSet;
 import fi.nls.oskari.wmts.domain.TileMatrixLink;
 import fi.nls.oskari.wmts.domain.WMTSCapabilities;
 import fi.nls.oskari.wmts.domain.WMTSCapabilitiesLayer;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class LayerJSONFormatterWMTS extends LayerJSONFormatter {
@@ -18,6 +21,48 @@ public class LayerJSONFormatterWMTS extends LayerJSONFormatter {
     @Deprecated
     public static JSONObject createCapabilitiesJSON(final WMTSCapabilities wmts,final WMTSCapabilitiesLayer layer) {
         return createCapabilitiesJSON(layer);
+    }
+
+    @Override
+    public JSONObject getJSON(OskariLayer layer,
+            final String lang,
+            final boolean isSecure) {
+
+        final JSONObject layerJson = getBaseJSON(layer, lang, isSecure);
+
+        JSONHelper.putValue(layerJson, "tileMatrixSetId",
+                getTileMatrixSetId(layer.getCapabilities(), layer.getSrs_name()));
+
+        // TODO: parse tileMatrixSetData for styles and set default style name from the one where isDefault = true
+        String styleName = layer.getStyle();
+
+        if(styleName == null || styleName.isEmpty()) {
+            styleName = "default";
+        }
+        JSONHelper.putValue(layerJson, "style", styleName);
+        JSONArray styles = new JSONArray();
+        // currently supporting only one style (default style)
+        styles.put(createStylesJSON(styleName, styleName, null));
+        JSONHelper.putValue(layerJson, "styles", styles);
+
+        // if options have urlTemplate -> use it (treat as a REST layer)
+        final String urlTemplate = JSONHelper.getStringFromJSON(layer.getOptions(), "urlTemplate", null);
+        final boolean needsProxy = useProxy(layer);
+        if(urlTemplate != null) {
+            if(needsProxy || isBeingProxiedViaOskariServer(layerJson.optString("url"))) {
+                // remove requestEncoding so we always get KVP params when proxying
+                JSONObject options = layerJson.optJSONObject("options");
+                options.remove("requestEncoding");
+            } else {
+                // setup tileURL for REST layers
+                final String originalUrl = layer.getUrl();
+                layer.setUrl(urlTemplate);
+                JSONHelper.putValue(layerJson, "tileUrl", layer.getUrl(isSecure));
+                // switch back the original url in case it's used down the line
+                layer.setUrl(originalUrl);
+            }
+        }
+        return layerJson;
     }
 
     public static JSONObject createCapabilitiesJSON(WMTSCapabilitiesLayer layer) {
